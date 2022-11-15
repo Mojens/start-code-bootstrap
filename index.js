ee8c7e1171f4738047396edbb9d9403f0c55034a
Mojens <moha135h@stud.kea.dk>
2022-11-15 16:20:36 +0100

    Template

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
 import {
-  loadHtml,
-  adjustForMissingHash,
-  setActiveLink,
-  renderTemplate,
+    loadHtml,
+    adjustForMissingHash,
+    setActiveLink,
+    renderTemplate,
 
 } from "./utils.js"
 
-import { load as loadV2 } from "./pages/page1/cars.js"
+import {load as loadV2} from "./pages/page1/cars.js"
 
 window.addEventListener("load", async () => {
-  const templateHome = await loadHtml("./pages/home/home.html")
-  const templateCarsBootstrap = await loadHtml("./pages/page1/page1.html")
+    const templateHome = await loadHtml("./pages/home/home.html")
+    const templatePage1 = await loadHtml("./pages/page1/page1.html")
+    const templatePage2 = await loadHtml("./pages/page2/page2.html")
+    const templatePage3 = await loadHtml("./pages/page3/page3.html")
+    const templatePage4 = await loadHtml("./pages/page4/page4.html")
 
-  const router = new Navigo("/", { hash: true });
-  window.router = router
+    const router = new Navigo("/", {hash: true});
+    window.router = router
 
-  adjustForMissingHash()
-  router
-    .hooks({
-      before(done, match) {
-        setActiveLink("topnav", match.url)
-        done()
-      }
-    })
-    .on({
-      "/": () => renderTemplate(templateHome, "content"),
+    adjustForMissingHash()
+    router
+        .hooks({
+            before(done, match) {
+                setActiveLink("topnav", match.url)
+                done()
+            }
+        })
+        .on({
+            "/": () => renderTemplate(templateHome, "content"),
+            "/page1": (match) => {
+                renderTemplate(templatePage1, "content")
+                loadV2(1, match)
+            },
+            "/page2": (match) => {
+                renderTemplate(templatePage2, "content")
+            },
+            "/page3": (match) => {
+                renderTemplate(templatePage3, "content")
+            },
 
-      "/page1": (match) => {
-        renderTemplate(templateCarsBootstrap, "content")
-        loadV2(1, match)
-      }
-    })
-    .notFound(() => renderTemplate("No page for this route found", "content"))
-    .resolve()
+            "/page4": (match) => {
+                renderTemplate(templatePage4, "content")
+            }
+        })
+        .notFound(() => renderTemplate("No page for this route found", "content"))
+        .resolve()
 });
 
 
